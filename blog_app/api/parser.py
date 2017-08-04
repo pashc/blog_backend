3b5ef0ff017576994e632f1985235511eb072792
Paul Werner <paul.werner@magicline.de>
2017-08-04 22:17:11 +0200

    adjust parser

--- blog_app/api/parser.py
+++ blog_app/api/parser.py
@@ -1,7 +1,6 @@
 from flask_restplus import reqparse
 
-request_parser = reqparse.RequestParser()
-request_parser.add_argument('page', type=int, required=False, default=1, help='page number')
-request_parser.add_argument('is_current', type=bool, required=False, default=True, help='is current page')
-request_parser.add_argument('per_page', type=int, required=False, choices=[2, 10, 20, 30, 40, 50],
-                            default=10, help='Results per page {error_msg}')
+pagination_parser = reqparse.RequestParser()
+pagination_parser.add_argument('page', type=int, required=False, default=1, help='page number')
+pagination_parser.add_argument('per_page', type=int, required=False, choices=[2, 10, 20, 30, 40, 50],
+                               default=10, help='Results per page {error_msg}')
