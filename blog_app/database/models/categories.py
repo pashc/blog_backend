72bdb8173a67820721067e9111b10390a113a861
Paul Werner <paul.werner@magicline.de>
2017-08-11 20:43:33 +0200

    add user model

--- blog_app/database/models/categories.py
+++ blog_app/database/models/categories.py
@@ -5,6 +5,7 @@ CATEGORY_ID = db.Sequence('category_id_seq', start=0)
 
 class Categories(db.Model):
     __tablename__ = 'categories'
+
     id = db.Column(db.Integer, CATEGORY_ID, primary_key=True, server_default=CATEGORY_ID.next_value())
     name = db.Column(db.String(50))
 
