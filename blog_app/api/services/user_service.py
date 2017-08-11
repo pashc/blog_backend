c6ecdd05c95683d5189d4474947a69a187a2e673
Paul Werner <paul.werner@magicline.de>
2017-08-11 23:53:10 +0200

    add user service

---
+++ blog_app/api/services/user_service.py
@@ -0,0 +1,36 @@
+from blog_app.database import db
+from blog_app.database.models.users import Users
+
+
+def find(user_id):
+    return Users.query.filter(Users.id == user_id).one()
+
+
+def create(data):
+    username = data.get('username')
+    password = data.get('password')
+
+    user = Users(username, password)
+
+    db.session.add(user)
+    db.session.commit()
+    return None, 201
+
+
+def update(user_id, data):
+    user = find(user_id)
+
+    user.username = data.get('username')
+    user.hash_password(data.get('password'))
+
+    db.session.add(user)
+    db.session.commit()
+    return None, 204
+
+
+def delete(user_id):
+    user = find(user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+    return None, 204
