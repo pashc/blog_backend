3f36dabb153737164884b9d31c5dc1932049c2ce
Paul Werner <paul.werner@magicline.de>
2017-08-11 19:43:38 +0200

    split model classes

--- blog_app/api/service.py
+++ blog_app/api/service.py
@@ -1,29 +1,30 @@
 from blog_app.database import db
-from blog_app.database.models import Article, Category
+from blog_app.database.models.articles import Articles
+from blog_app.database.models.categories import Categories
 
 
 def create_article(data):
     title = data.get('title')
     content = data.get('content')
     category_id = data.get('category_id')
-    category = Category.query.filter(Category.id == category_id).one()
-    article = Article(title, content, category)
+    category = Categories.query.filter(Categories.id == category_id).one()
+    article = Articles(title, content, category)
     db.session.add(article)
     db.session.commit()
 
 
 def update_article(article_id, data):
-    article = Article.query.filter(Article.id == article_id).one()
+    article = Articles.query.filter(Articles.id == article_id).one()
     article.title = data.get('title')
     article.content = data.get('content')
     category_id = data.get('category_id')
-    article.category = Category.query.filter(Category.id == category_id).one()
+    article.category = Categories.query.filter(Categories.id == category_id).one()
     db.session.add(article)
     db.session.commit()
 
 
 def delete_article(article_id):
-    article = Article.query.filter(Article.id == article_id).one()
+    article = Articles.query.filter(Articles.id == article_id).one()
     db.session.delete(article)
     db.session.commit()
 
@@ -31,22 +32,22 @@ def delete_article(article_id):
 def create_category(data):
     name = data.get('name')
     category_id = data.get('id')
-    category = Category(name)
+    category = Categories(name)
     if category_id:
         category.id = category_id
 
     db.session.add(category)
-    db.session.commit()
+    return db.session.commit()
 
 
 def update_category(category_id, data):
-    category = Category.query.filter(Category.id == category_id).one()
+    category = Categories.query.filter(Categories.id == category_id).one()
     category.name = data.get['name']
     db.sesson.add(category)
     db.session.commit()
 
 
 def delete_category(category_id):
-    category = Category.query.filter(Category.id == category_id).one()
+    category = Categories.query.filter(Categories.id == category_id).one()
     db.session.delete(category)
     db.session.commit()
