dd55c9cf366b72e077ca73743acbc8be06fa97b1
Paul Werner <paulwerner@protonmail.com>
2018-01-16 22:32:45 +0100

    refactor persistence layer

--- blog_app/database/models/blog/article.py
+++ blog_app/database/models/blog/article.py
@@ -1,35 +1,26 @@
-from datetime import datetime
 import time
+from datetime import datetime
+
 from blog_app.database import db
 
 ARTICLE_ID = db.Sequence('article_id_seq', start=0)
 
 
 class Article(db.Model):
-    __tablename__ = 'articles'
+    __tablename__ = 'article'
 
     id = db.Column(db.Integer, primary_key=True, server_default=ARTICLE_ID.next_value())
     title = db.Column(db.String(80))
     content = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime)
-
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    category = db.relationship('Category', backref=db.backref('articles', lazy='dynamic'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    pub_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def to_dict(self):
         return dict(id=self.id,
                     title=self.title,
                     content=self.content,
                     pub_date=time.mktime(self.pub_date.timetuple()),
-                    category=self.category.name if self.category else None)
-
-    def __init__(self, title, content, category, pub_date=None):
-        self.title = title
-        self.content = content
-        self.category = category
-        if pub_date is None:
-            pub_date = datetime.utcnow()
-        self.pub_date = pub_date
+                    category_id=self.category_id)
 
     def __repr__(self):
         return '<Article %r>' % self.title
--- blog_app/database/models/blog/category.py
+++ blog_app/database/models/blog/category.py
@@ -4,16 +4,15 @@ CATEGORY_ID = db.Sequence('category_id_seq', start=0)
 
 
 class Category(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = 'category'
 
     id = db.Column(db.Integer, primary_key=True, server_default=CATEGORY_ID.next_value())
     name = db.Column(db.String(50))
 
+    articles = db.relationship('Article', backref='category', lazy='dynamic')
+
     def to_dict(self):
         return dict(id=self.id, name=self.name)
-
-    def __init__(self, name):
-        self.name = name
 
     def __repr__(self):
         return '<Category %r>' % self.name
